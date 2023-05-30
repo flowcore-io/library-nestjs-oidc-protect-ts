4b2b085914d4eb6589295a2588e03fbb2dcf0a28
flowcore-platform <gitops@flowcore.io>
2023-05-30 13:55:44 +0100

    feat: added back in configurable stored in variable

--- src/library/decorator/authenticated-user.decorator.ts
+++ src/library/decorator/authenticated-user.decorator.ts
@@ -3,18 +3,19 @@ import { OidcProtectService } from "../oidc-protect/oidc-protect.service";
 
 export interface AuthenticatedUserOptions {
   required?: boolean;
+  storedIn?: string;
 }
 
 export const AuthenticatedUser = createParamDecorator<AuthenticatedUserOptions>(
   async (data, ctx: ExecutionContext) => {
     const request = await OidcProtectService.getRequest(ctx);
 
-    const { required = true } = data || {};
+    const { required = true, storedIn = "authenticatedUser" } = data || {};
 
-    if (!request.authenticatedUser && required) {
+    if (!request[storedIn] && required) {
       throw new Error("AuthenticatedUser not found");
     }
 
-    return request.authenticatedUser;
+    return request[storedIn];
   },
 );
