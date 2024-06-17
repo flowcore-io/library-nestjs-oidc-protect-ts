8f76c53c16a0ce1c8cda934acaae049dbd7e47e7
flowcore-platform <gitops@flowcore.io>
2024-06-17 13:41:34 +0100

    feat: add oidc-protect service to library index

--- src/library/index.ts
+++ src/library/index.ts
@@ -3,4 +3,5 @@ export * from "./interface/oidc-protect-module-options.interface";
 export * from "./config/oidc-protect.configuration";
 export * from "./guard";
 export * from "./decorator";
+export * from "./oidc-protect/oidc-protect.service";
 export * from "./oidc-protect.module";
